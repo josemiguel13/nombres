b31568aa3b826bcef88c1ceb0c7aa6f2f22cf66a
josemiguel13 <josecely2009@gmail.com>
2015-09-28 22:56:21 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,47 @@
+window.onload = function()
+{
+    var canvas = document.getElementById("canvas");
+    var context = canvas.getContext('2d');
+
+
+    var puntosValor = [[40, 30, 280, 30],
+                       [175, 30, 175, 250],
+                       [30, 250, 180, 250],
+
+                       [320, 30, 500, 30],
+                       [320, 30, 320, 250],
+                       [500, 30, 500, 250],
+                       [320, 250, 500, 250],
+
+                       [720, 30, 520, 125],
+                       [520, 130, 710, 130],
+                       [520, 250, 710, 130],
+
+                       [740, 30, 950, 30],
+                       [740, 250, 950, 250],
+                       [740, 30, 740, 255],
+                       [740, 130, 910, 130],
+                       ];
+        
+    var puntosnombre = function(puntos)
+    {
+        context.beginPath();
+        context.moveTo(puntos[0], puntos[1]);
+        context.lineTo(puntos[2], puntos[3]);
+        context.lineWidth = 10;
+        context.strokeStyle = randomColor();
+        context.stroke();
+    };
+
+    for(var i = 0; i < puntosValor.length; i++)
+    {
+        puntosnombre(puntosValor[i]);
+    }
+    function randomColor()
+	{
+    	// from http://www.paulirish.com/2009/random-hex-color-code-snippets/
+    	return '#'+(function lol(m,s,c){return s[m.floor(m.random() * s.length)] +
+    	(c && lol(m,s,c-1));})(Math,'0123456789ABCDEF',4);
+  	};
+
+};
